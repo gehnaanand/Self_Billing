6b029b9d41d7a11114a1ebb200511ded66aee059
vkdhanaraj <vk.dhanaraj@gmail.com>
2019-10-14 18:37:49 +0530

    implemented Store_grid

---
+++ app/src/main/java/com/example/self_billing/RecyclerViewClickListener.java
@@ -0,0 +1,7 @@
+package com.example.self_billing;
+
+import android.view.View;
+
+public interface RecyclerViewClickListener {
+    void recyclerViewListClicked(View v, String StoreName);
+}
--- app/src/main/java/com/example/self_billing/Store_grid.java
+++ app/src/main/java/com/example/self_billing/Store_grid.java
@@ -3,12 +3,20 @@ package com.example.self_billing;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Store_grid extends AppCompatActivity {
+public class Store_grid extends AppCompatActivity implements RecyclerViewClickListener {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    Store_Adapter mAdapter;
+    String[] Stores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +32,24 @@ public class Store_grid extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        recyclerView =findViewById(R.id.rvStores);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        Stores = new String[]{"Store1","Store2","Store3","Store4","Store5","Store6","Store7","Store1","Store2","Store3","Store4","Store5","Store6","Store7"};
+        mAdapter = new Store_Adapter(Stores,  this);
+        recyclerView.setAdapter(mAdapter);
     }
+
+    @Override
+    public void recyclerViewListClicked(View v, String StoreName){
+        Intent intent = new Intent(Store_grid.this,Add_Item.class);
+        intent.putExtra("StoreName",StoreName);
+        startActivity(intent);
+    }
+
 }
